57b9caa2b6b5b75d5f6956f2bf2da1769fe12d97
samuel <samuelmmanor@gmail.com>
2022-01-02 22:00:16 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(x, y) {
+    return x + y;
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x, y) {
+    return x / y;
+}
+
+function increment(x) {
+    x += 1;
+    return x;
+}
+
+function decrement(x) {
+    x -= 1;
+    return x;
+}
+
+function makeInt(string) {
+    return parseInt(string, 10);
+}
+
+function preserveDecimal(string) {
+    return parseFloat(string);
+}
